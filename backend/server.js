ac4f799d5de47ee251d8ba5c98728bb046fca256
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-01-30 12:58:01 +0530

    implement user registration from UI, sending form data to backend API for registration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 4000;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
